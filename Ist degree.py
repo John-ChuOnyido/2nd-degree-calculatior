7a653c68aeb94b2649060ec0b0d70586941318e1
John-ChuOnyido <64579999+John-ChuOnyido@users.noreply.github.com>
2020-05-18 01:09:47 +0100

    Create Ist degree.py

---
+++ Ist degree.py
@@ -0,0 +1,9 @@
+This program solves algebraic expression where the coefficient of x on the right is 0.
+def first_degree():
+	y = -50
+	while y < 50:
+		if 2*y + 5 == 13:
+			print("Y =", y)
+		y += 1
+		#calling the function
+first_degree()
